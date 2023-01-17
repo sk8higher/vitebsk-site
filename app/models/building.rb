df14bd77073368d91c1405ea4e9acb77b342a0df
Nelly Kim <sk8high3r@ya.ru>
2023-01-17 21:08:11 +0300

    Add Buildings model with migration

---
+++ app/models/building.rb
@@ -0,0 +1,6 @@
+class Building < ApplicationRecord
+  has_one_attached :photo
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 70 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 255 }
+end
