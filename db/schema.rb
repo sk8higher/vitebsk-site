5e890f0f7cae2e9de873b8ac13329efab4862606
Nelly Kim <sk8high3r@ya.ru>
2023-01-19 22:46:15 +0300

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_155758) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_193507) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_155758) do
   end
 
   create_table "buildings", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "monuments", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
