6be1a2c58d8213f7eedc44a40ef70f0a7322bb8a
Nelly Kim <sk8high3r@ya.ru>
2023-01-19 22:44:42 +0300

    Add validations to Monument model

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -1,3 +1,9 @@
 class Monument < ApplicationRecord
+  include Pagy::Backend
+
   has_one_attached :photo
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 70 }
+  validates :description, presence: true, length: { minimum: 5 }
+  validates :photo, presence: true
 end
