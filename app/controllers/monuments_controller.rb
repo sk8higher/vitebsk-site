a16a5655257e4869d06e26fcd09d98a7a5510efe
Nelly Kim <sk8high3r@ya.ru>
2023-01-21 13:55:12 +0300

    Add monumentscontroller and index view

---
+++ app/controllers/monuments_controller.rb
@@ -0,0 +1,51 @@
+class MonumentsController < ApplicationController
+  before_action :set_monument, only: %i[show edit update destroy]
+
+  def index
+    @pagy, @monuments = pagy(Monument.all, items: 12)
+  end
+
+  def show
+  end
+
+  def new
+    @monument = Monument.new
+  end
+
+  def edit
+  end
+
+  def create
+    @monument = Monument.new(monument_params)
+
+    if @monument.save
+      redirect_to monument_path(@monument)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @monument.update(monument_params)
+      redirect_to monument_path(@monument)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @monument.destroy
+
+    redirect_to monuments_url, notice: 'Статья была успешно уничтожена.'
+  end
+
+  private
+
+  def set_monument
+    @monument = Monument.find(params[:id])
+  end
+
+  def monument_params
+    params.require(:monument).permit(:name, :description, :photo)
+  end
+end
