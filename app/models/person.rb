f80b29192251cea62f281baabccb334191d74ff9
Nelly Kim <sk8high3r@ya.ru>
2023-01-23 22:57:35 +0300

    Add validations to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,11 @@
 class Person < ApplicationRecord
+  include Pagy::Backend
+
   has_many :artworks
 
   has_one_attached :photo
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 70 }
+  validates :bio, presence: true, length: { minimum: 5 }
+  validates :photo, presence: true
 end
