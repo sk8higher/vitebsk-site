46c8bf48db39fc007c1eced98ca00097cb4593f7
Nelly Kim <sk8high3r@ya.ru>
2023-01-23 23:00:56 +0300

    Add validations to artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,6 @@
 class Artwork < ApplicationRecord
   has_one_attached :photo
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :descripton, presence: true, length: { minimum: 5 }
 end
