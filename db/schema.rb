b9857eb72eb1c24cb5cdf6014c01c87c5cced1f6
Nelly Kim <sk8high3r@ya.ru>
2023-01-23 10:04:19 +0300

    Create Person model with a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_160046) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_070334) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_160046) do
   create_table "museums", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
