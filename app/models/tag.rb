dea23e356d486ac5f77f61e45c7280b1614397cb
Nelly Kim <sk8high3r@ya.ru>
2023-01-24 10:14:18 +0300

    Add Taggings model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
   has_many :taggings
-  has_many :posts, through: :taggings
+  has_many :people, through: :taggings
 end
--- db/migrate/20230124071256_create_taggings.rb
+++ db/migrate/20230124071256_create_taggings.rb
@@ -2,7 +2,7 @@ class CreateTaggings < ActiveRecord::Migration[7.0]
   def change
     create_table :taggings do |t|
       t.belongs_to :tag, null: false, foreign_key: true
-      t.belongs_to :post, null: false, foreign_key: true
+      t.belongs_to :person, null: false, foreign_key: true
 
       t.timestamps
     end
