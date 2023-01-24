a70c185a198879e7331e8b803517da2fa1a7d1a4
Nelly Kim <sk8high3r@ya.ru>
2023-01-24 09:59:07 +0300

    Add CreateTags migration with Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_063920) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_065811) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -72,6 +72,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_063920) do
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
