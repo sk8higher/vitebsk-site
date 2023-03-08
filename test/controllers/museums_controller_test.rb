ac80f39d7a24fdb0b1bfa427ab18349f6711db5e
Nelly Kim <sk8high3r@ya.ru>
2023-03-08 16:53:30 +0300

    Remove generated tests

--- test/controllers/museums_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class MuseumsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/monument_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class MonumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
