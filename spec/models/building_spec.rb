363229963e01e66589bde2de7bf3d52f33aafcfd
Nelly Kim <sk8high3r@ya.ru>
2023-03-08 17:38:05 +0300

    Add base tests for Building model

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -1,8 +1,28 @@
+require_relative '../rails_helper'
+
 RSpec.describe Building do
   let(:building) { Building.new }
 
   context 'model validations' do
-    it 'should have one photo attached' do
+    it { should have_one_attached :photo }
+
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(70) }
+
+    it { should validate_length_of(:description).is_at_least(5) }
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:description) }
+
+    it { should validate_presence_of(:photo) }
+  end
+
+  context 'model object validations' do
+    it 'should be invalid when just instantiated' do
+      expect(building).not_to be_valid
+    end
+
+    it 'should be invalid with no name provided' do
     end
   end
 end
