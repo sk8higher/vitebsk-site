cd06d6606d7f59aa6ccef16f23c04d92b16259a2
Nelly Kim <sk8high3r@ya.ru>
2023-03-08 19:04:43 +0300

    Write tests for Building model

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../rails_helper'
+require 'faker'
 
 RSpec.describe Building do
-  let(:building) { create(:building)}
-
+  let(:building) { create(:building) }
+  
   context 'model validations' do
     it { should have_one_attached :photo }
 
@@ -17,9 +18,52 @@ RSpec.describe Building do
     it { should validate_presence_of(:photo) }
   end
 
-  context 'model object validations' do
+  describe 'model object validations' do
     it 'should be valid with all attributes present' do
       expect(building).to be_valid
+    end
+
+    context 'attribute presence' do
+      it 'should not be valid without photo' do
+        building.photo = nil
+        expect(building).not_to be_valid
+      end
+
+      it 'should not be valid without name' do
+        building.name = nil
+        expect(building).not_to be_valid
+      end
+
+      it 'should not be valid without description' do
+        building.description = nil
+        expect(building).not_to be_valid
+      end
+    end
+
+    describe 'attribute length' do
+      context 'name length validation' do
+        it 'should not be valid with a name less than 3 symbols' do
+          building.name = Faker::Lorem.characters(number: 2)
+          expect(building).not_to be_valid
+        end
+
+        it 'should not be valid with a name greater than 70 symbols' do
+          building.name = Faker::Lorem.characters(number: 75)
+          expect(building).not_to be_valid
+        end
+      end
+
+      context 'description length validation' do
+        it 'should not be valid with a name less than 5 symbols' do
+          building.description = Faker::Lorem.characters(number: 3)
+          expect(building).not_to be_valid
+        end
+
+        it 'should be valid with any number of symbols greater than 5' do
+          building.description = Faker::Lorem.characters(number: 100)
+          expect(building).to be_valid
+        end
+      end
     end
   end
 end
