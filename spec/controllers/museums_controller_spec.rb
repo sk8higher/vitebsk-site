ba0f1682a21a14e41eab9644bbe3aabd97eb83bb
Nelly Kim <sk8high3r@ya.ru>
2023-03-15 23:02:09 +0300

    Add museum controller spec

---
+++ spec/controllers/museums_controller_spec.rb
@@ -0,0 +1,74 @@
+require_relative '../rails_helper'
+
+RSpec.describe MuseumsController do
+  let(:saved_museum) { create(:museum) }
+  let(:created_museum) { build(:museum) }
+
+  describe 'GET #index' do
+    it 'returns http success and renders the index template' do
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success and renders the show template' do
+      get :show, params: { id: saved_museum.id }
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'returns http success and renders the new template' do
+      get :new
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new museum and redirects to the show page' do
+      file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'image3.jpg'), 'image/png')
+      expect {
+        post :create, params: { museum: { name: created_museum.name,
+                                         description: created_museum.description,
+                                         photo: file } }
+      }.to change(Museum, :count).by(1)
+      expect(response).to redirect_to(assigns(:museum))
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'returns http success and renders the edit template' do
+      get :edit, params: { id: saved_museum.id }
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'updates the museum and redirects to the show page' do
+      file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'image2.png'), 'image/png')
+      new_description = Faker::Lorem.characters(number: 40)
+
+      patch :update, params: { id: saved_museum.id, museum: { name: Faker::Lorem.characters(number: 40),
+                                                             description: new_description,
+                                                             photo: file } }
+      expect(response).to redirect_to(assigns(:museum))
+      saved_museum.reload
+      expect(saved_museum.description).to eq(new_description)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deletes the museum and redirects to the index page' do
+      delete_museum = create(:museum)
+      expect {
+        delete :destroy, params: { id: delete_museum.id }
+      }.to change(Museum, :count).by(-1)
+      expect(response).to redirect_to(museums_path)
+    end
+  end
+end
