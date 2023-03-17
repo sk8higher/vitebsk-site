2c713d0a28e9a861a3947d378333c5b81a6fb8ec
Nelly Kim <sk8high3r@ya.ru>
2023-03-17 18:14:29 +0300

    Reformat test, add test for incorrect update to PeopleController spec

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -32,12 +32,12 @@ RSpec.describe PeopleController do
   describe 'POST #create' do
     it 'creates a new person and redirects to the show page' do
       file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'image4.jpg'), 'image/png')
-      expect {
+      expect do
         post :create, params: { person: { name: created_person.name,
-                                         bio: created_person.bio,
-                                         photo: file,
-                                         tag_id: tag.id } }
-      }.to change(Person, :count).by(1)
+                                          bio: created_person.bio,
+                                          photo: file,
+                                          tag_id: tag.id } }
+      end.to change(Person, :count).by(1)
       expect(response).to redirect_to(assigns(:person))
     end
   end
@@ -56,20 +56,30 @@ RSpec.describe PeopleController do
       new_bio = Faker::Lorem.characters(number: 40)
 
       patch :update, params: { id: saved_person.id, person: { name: Faker::Lorem.characters(number: 40),
-                                                             bio: new_bio,
-                                                             photo: file } }
+                                                              bio: new_bio,
+                                                              photo: file } }
       expect(response).to redirect_to(assigns(:person))
       saved_person.reload
       expect(saved_person.bio).to eq(new_bio)
+    end
+
+    it 'returns unprocessable entity if person is not updated' do
+      file = fixture_file_upload(Rails.root.join('spec', 'fixtures', 'image2.png'), 'image/png')
+      patch :update, params: { id: saved_person.id, person: { name: nil,
+                                                              bio: nil,
+                                                              photo: file } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to render_template(:edit)
     end
   end
 
   describe 'DELETE #destroy' do
     it 'deletes the person and redirects to the index page' do
       delete_person = create(:person, tag_id: tag.id)
-      expect {
+      expect do
         delete :destroy, params: { id: delete_person.id }
-      }.to change(Person, :count).by(-1)
+      end.to change(Person, :count).by(-1)
       expect(response).to redirect_to(people_path)
     end
   end
