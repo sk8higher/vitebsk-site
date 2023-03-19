0169d10752fe2e3cc3d2004ae9aff151869900cb
Nelly Kim <sk8high3r@ya.ru>
2023-03-20 01:02:58 +0300

    Add locale scope to routes and methods to change locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :buildings, :monuments, :museums
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
+    resources :buildings, :monuments, :museums
 
-  resources :people do
-    resources :artworks
+    resources :people do
+      resources :artworks
+    end
+
+    root 'pages#index'
   end
-
-  root 'pages#index'
 end
